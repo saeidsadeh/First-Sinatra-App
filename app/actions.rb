131c80bf55aa1f1675a320d5d60a3dc2199758a4
saeid sadeh <saeid.sadeh@yahoo.com>
2016-06-12 21:30:26 +0000

    Message details page

--- app/actions.rb
+++ app/actions.rb
@@ -12,3 +12,28 @@ end
 get '/messages/new' do
   erb :'messages/new'
 end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title:   params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
+
+get '/person' do
+  @list = Message.all
+  erb :saeid
+end
+
+get '/jigar' do
+  @list = Message.all
+  erb :azadeh
+end
